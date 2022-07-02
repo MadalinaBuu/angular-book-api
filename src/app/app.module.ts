fb14ed9db42e90c491a63b5dd4b8f6264930e19b
Madalina Buu <madalina.ciucioiu@gmail.com>
2022-07-02 11:47:12 +0300

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule }  from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import { ShowBookComponent } from './book/show-book/show-book.component';
+import { AddEditBookComponent } from './book/add-edit-book/add-edit-book.component';
+import { BookApiService } from './book-api.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    ShowBookComponent,
+    AddEditBookComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [BookApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
